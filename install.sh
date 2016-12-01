171cc1a43423bdb34530ccf3730271c88f1fcbab
James Fisher <fisher14@gmail.com>
2016-12-01 13:45:03 -0500

    Some updates

---
+++ install.sh
@@ -0,0 +1,5 @@
+ln -s ~/dotfiles/atom/config.cson ~/.atom/config.cson
+ln -s ~/dotfiles/atom/styles.less ~/.atom/styles.less
+ln -s ~/dotfiles/atom/init.coffee ~/.atom/init.coffee
+ln -s ~/dotfiles/atom/keymap.cson ~/.atom/keymap.cson
+ln -s ~/dotfiles/atom/snippets.cson ~/.atom/snippets.cson
